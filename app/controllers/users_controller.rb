8a667b0c7bbcd0dcb649c979b7a5d53b738b701e
apple <apple@apples-MacBook-Pro.local>
2020-06-30 21:11:04 +0200

    add edit feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+ end
+
+  def edit
+    @user = User.find(params[:id])
+   end
+
+  def show; end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
 
   def create
     @user = User.new(user_params)
@@ -9,5 +26,15 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
