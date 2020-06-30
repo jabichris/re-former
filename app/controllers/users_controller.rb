23474a8889b4946eb2cacd8d41dd8f66de0c1beb
apple <apple@apples-MacBook-Pro.local>
2020-06-30 19:47:32 +0200

    ✨ NEW FEATURE: add create user feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+end
